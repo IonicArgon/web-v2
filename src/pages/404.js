58abd8d17d8aa7f5e4b7aa05b69a8a6ea3b19502
Marco Tan <marco.tan.200405@gmail.com>
2022-03-15 20:53:37 -0400

    FINAL COMMIT before i make all the blog and project pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -28,7 +28,9 @@ const NotFound = () => {
         <Divider my={6} />
         <Box my={6} align="center">
           <NextLink href="/" passHref>
-            <Button colorScheme="teal">Return to home</Button>
+            <Button bgColor="#8ec07c" color="#282828">
+              Return to home
+            </Button>
           </NextLink>
         </Box>
       </Container>
--- src/pages/construction.js
+++ src/pages/construction.js
@@ -31,7 +31,9 @@ const Construction = () => {
         <Divider my={6} />
         <Box my={6} align="center">
           <NextLink href="/" passHref>
-            <Button colorScheme="teal">Return to home</Button>
+            <Button bgColor="#8ec07c" color="#282828">
+              Return to home
+            </Button>
           </NextLink>
         </Box>
       </Container>
