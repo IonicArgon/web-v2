d97cbe28c201883a5927f3983878279d40613355
Marco Tan <marco.tan.200405@gmail.com>
2022-03-15 20:47:33 -0400

    added noscript

--- src/components/layouts/main.js
+++ src/components/layouts/main.js
@@ -5,7 +5,8 @@ import { Box, Container } from "@chakra-ui/react";
 
 const Main = ({ children, router }) => {
   return (
-    <Box as="main" pb={8}>
+    <>
+      <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
@@ -31,12 +32,24 @@ const Main = ({ children, router }) => {
 
       <NavBar path={router.asPath} />
 
+      <noscript>
+        <h1 style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}>
+          Please enable JavaScript to view this website.
+        </h1>
+      </noscript>
+
       <Container maxW="container.lg" pt={20}>
         {children}
 
         <Footer />
       </Container>
     </Box>
+    </>
   );
 };
 
