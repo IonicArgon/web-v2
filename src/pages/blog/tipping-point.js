ec6810c2b78e4c3960edfb46dc27e9844b1f21b0
Marco Tan <marco.tan.200405@gmail.com>
2022-03-15 20:19:54 -0400

    forgot these pages didn't return anything yet

---
+++ src/pages/blog/tipping-point.js
@@ -0,0 +1,13 @@
+import { Container } from "@chakra-ui/react";
+import Layout from "../../components/layouts/article";
+
+const Project = () => {
+    return(
+        <Layout>
+            <Container>
+            </Container>
+        </Layout>
+    );
+}
+
+export default Project;
