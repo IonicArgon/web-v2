dd3a4ba772c216699b55b94f484c849cb27e92a2
Marco Tan <marco.tan.200405@gmail.com>
2023-01-04 23:42:40 -0500

    want 2 c if this metadata works

--- src/components/layouts/article.js
+++ src/components/layouts/article.js
@@ -21,6 +21,23 @@ const Layout = ({ children, title }) => {
       <>
         {title && (
           <Head>
+            <meta property="og:title" content="{title} - Marco Tan" />
+            <meta property="og:site_name" content="Marco Tan's Homepage" />
+            <meta property="og:url" content="https://ionicargon.vercel.app/" />
+            <meta property="og:type" content="website" />
+            <meta property="og:image" content="/images/profile.jpg" />
+
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta
+              property="twitter:url"
+              content="https://ionicargon.vercel.app/"
+            />
+            <meta property="twitter:title" content="{title} - Marco Tan" />
+            <meta
+              property="twitter:description"
+              content="Hello, I'm Marco Tan!"
+            />
+            <meta property="twitter:image" content="/images/profile.jpg" />
             <title>{title} - Marco Tan</title>
           </Head>
         )}
